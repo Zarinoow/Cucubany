2a44095bee37b3bb12eef905e52f120630e6f03f
Zarinoow <77458474+Zarinoow@users.noreply.github.com>
2025-09-17 01:44:57 +0200

    Ram bug fix

--- Launcher/CucubanyOptions.cs
+++ Launcher/CucubanyOptions.cs
@@ -1,236 +1,165 @@
-﻿using System;
+﻿// Fichier: Launcher/CucubanyOptions.cs
+using System;
 using System.IO;
+using System.Collections.Generic;
 using CmlLib.Core;
 using Newtonsoft.Json;
 
-namespace Cucubany.Launcher;
-
-public class CucubanyOptions : MLaunchOption
+namespace Cucubany.Launcher
 {
-    /*
-     * Options: 
-     * RAM (MaximumRamMb, MinimumRamMb) ✅
-     * JavaPath (JavaPath) ✅
-     * ScreenSize (ScreenWidth, ScreenHeight, FullScreen) ✅
-     * Session (Session) ✅
-     */
-    private CucubanyPath Path;
-    
-    /*
-     * Customs variables:
-     */
-    private bool useCustomJavaPath = false;
-    public string CustomJavaPath { get; set; }
-    public string LastConnectedAccount { get; set; }
-    
-    
-    public CucubanyOptions(CucubanyPath path)
+    public class CucubanyOptions : MLaunchOption
     {
-        Path = path;
-        
-        Load();
-        
-        VersionType = "Cucubany";
-        GameLauncherName = "Cucubany";
-        GameLauncherVersion = "2";
-        JVMArguments = new[] { 
-            "-XX:+UnlockExperimentalVMOptions",
-            "-XX:+UseG1GC",
-            "-XX:G1NewSizePercent=20",
-            "-XX:G1ReservePercent=20",
-            "-XX:MaxGCPauseMillis=50",
-            "-XX:G1HeapRegionSize=16M",
-            "-Djava.net.preferIPv4Stack=true"
-        };
-    }
-    
-    /*
-     * Data
-     */
-    
-    public void Save()
-    {
-        String path = Path.BasePath + "/launcher_settings.json";
-        
-        if(File.Exists(path)) File.Delete(path);
-        
-        using (StreamWriter file = File.AppendText(path))
-        using (JsonTextWriter writer = new JsonTextWriter(file))
+        private CucubanyPath Path;
+        private bool useCustomJavaPath = false;
+        public string CustomJavaPath { get; set; }
+        public string LastConnectedAccount { get; set; }
+
+        public CucubanyOptions(CucubanyPath path)
         {
-            writer.Formatting = Formatting.Indented;
+            Path = path;
+            Load();
+            VersionType = "Cucubany";
+            GameLauncherName = "Cucubany";
+            GameLauncherVersion = "2";
+            RebuildJvmArgs();
+        }
 
-            // Start writing
+        private void RebuildJvmArgs()
+        {
+            var args = new List<string>
+            {
+                $"-Xms{MinimumRamMb}M",
+                $"-Xmx{MaximumRamMb}M",
+                "-XX:+UnlockExperimentalVMOptions",
+                "-XX:+UseG1GC",
+                "-XX:G1NewSizePercent=20",
+                "-XX:G1ReservePercent=20",
+                "-XX:MaxGCPauseMillis=50",
+                "-XX:G1HeapRegionSize=16M",
+                "-Djava.net.preferIPv4Stack=true"
+            };
+            JVMArguments = args.ToArray();
+        }
+
+        public void SetMemory(int minMb, int maxMb)
+        {
+            MinimumRamMb = minMb;
+            MaximumRamMb = maxMb;
+            RebuildJvmArgs();
+            Save();
+        }
+
+        public void Save()
+        {
+            string path = Path.BasePath + "/launcher_settings.json";
+            if (File.Exists(path)) File.Delete(path);
+            using var file = File.AppendText(path);
+            using var writer = new JsonTextWriter(file) { Formatting = Formatting.Indented };
             writer.WriteStartObject();
-
-            // Writing RAM settings
-            writer.WritePropertyName("MinimumRamMb");
-            writer.WriteValue(MinimumRamMb);
-
-            writer.WritePropertyName("MaximumRamMb");
-            writer.WriteValue(MaximumRamMb);
-            
-            // Writing Screen settings
+            writer.WritePropertyName("MinimumRamMb"); writer.WriteValue(MinimumRamMb);
+            writer.WritePropertyName("MaximumRamMb"); writer.WriteValue(MaximumRamMb);
             writer.WritePropertyName("ScreenSize");
             writer.WriteStartObject();
-           
-            writer.WritePropertyName("ScreenWidth");
-            writer.WriteValue(ScreenWidth);
-            
-            writer.WritePropertyName("ScreenHeight");
-            writer.WriteValue(ScreenHeight);
-            
-            writer.WritePropertyName("FullScreen");
-            writer.WriteValue(FullScreen);
-            
+            writer.WritePropertyName("ScreenWidth"); writer.WriteValue(ScreenWidth);
+            writer.WritePropertyName("ScreenHeight"); writer.WriteValue(ScreenHeight);
+            writer.WritePropertyName("FullScreen"); writer.WriteValue(FullScreen);
             writer.WriteEndObject();
-            
-            // Writing Java path
             writer.WritePropertyName("CustomJavaPath");
             writer.WriteStartObject();
-            
-            writer.WritePropertyName("UseCustomJavaPath");
-            writer.WriteValue(useCustomJavaPath);
-            
-            writer.WritePropertyName("JavaPath");
-            writer.WriteValue(CustomJavaPath);
-            
+            writer.WritePropertyName("UseCustomJavaPath"); writer.WriteValue(useCustomJavaPath);
+            writer.WritePropertyName("JavaPath"); writer.WriteValue(CustomJavaPath);
             writer.WriteEndObject();
-            
-            // Last session
-            writer.WritePropertyName("lastConnectedAccount");
-            writer.WriteValue(LastConnectedAccount);
-            
-            if(MainWindow.KonamiCodeEnabled)
+            writer.WritePropertyName("lastConnectedAccount"); writer.WriteValue(LastConnectedAccount);
+            if (MainWindow.KonamiCodeEnabled)
             {
                 writer.WritePropertyName("KonamiCodeEnabled");
-                writer.WriteValue(MainWindow.KonamiCodeEnabled);
+                writer.WriteValue(true);
             }
-            
-            // End writing
             writer.WriteEndObject();
         }
-    }
-    
-    public void Load()
-    {
-        String path = Path.BasePath + "/launcher_settings.json";
 
-        if (!File.Exists(path))
+        public void Load()
         {
-            MinimumRamMb = 512;
-            MaximumRamMb = 4096;
-            ScreenWidth = 854;
-            ScreenHeight = 480;
-            FullScreen = false;
-            Save();
-        } 
-        else
-        {
-            using (StreamReader file = File.OpenText(path))
+            string path = Path.BasePath + "/launcher_settings.json";
+            if (!File.Exists(path))
             {
-                using (JsonTextReader reader = new JsonTextReader(file))
+                MinimumRamMb = 512;
+                MaximumRamMb = 4096;
+                ScreenWidth = 854;
+                ScreenHeight = 480;
+                FullScreen = false;
+                Save();
+            }
+            else
+            {
+                using var file = File.OpenText(path);
+                using var reader = new JsonTextReader(file);
+                while (reader.Read())
                 {
-                    while (reader.Read())
+                    if (reader.TokenType != JsonToken.PropertyName) continue;
+                    string name = reader.Value.ToString();
+                    reader.Read();
+                    switch (name)
                     {
-                        if (reader.TokenType == JsonToken.PropertyName)
-                        {
-                            String name = reader.Value.ToString();
+                        case "MinimumRamMb":
+                            MinimumRamMb = Convert.ToInt32(reader.Value);
+                            break;
+                        case "MaximumRamMb":
+                            MaximumRamMb = Convert.ToInt32(reader.Value);
+                            break;
+                        case "ScreenSize":
                             reader.Read();
-                            
-                            switch (name)
+                            while (reader.TokenType != JsonToken.EndObject)
                             {
-                                case "MinimumRamMb":
-                                    MinimumRamMb = int.Parse(reader.Value.ToString());
-                                    break;
-                                case "MaximumRamMb":
-                                    MaximumRamMb = int.Parse(reader.Value.ToString());
-                                    break;
-                                case "ScreenSize":
-                                    reader.Read();
-                                    while (reader.TokenType != JsonToken.EndObject)
-                                    {
-                                        String screenName = reader.Value.ToString();
-                                        reader.Read();
-                                        
-                                        switch (screenName)
-                                        {
-                                            case "ScreenWidth":
-                                                ScreenWidth = int.Parse(reader.Value.ToString());
-                                                break;
-                                            case "ScreenHeight":
-                                                ScreenHeight = int.Parse(reader.Value.ToString());
-                                                break;
-                                            case "FullScreen":
-                                                FullScreen = bool.Parse(reader.Value.ToString());
-                                                break;
-                                        }
-                                        reader.Read();
-                                    }
-                                    break;
-                                case "CustomJavaPath":
-                                    reader.Read();
-                                    while (reader.TokenType != JsonToken.EndObject)
-                                    {
-                                        String javaName = reader.Value.ToString();
-                                        reader.Read();
-                                        
-                                        switch (javaName)
-                                        {
-                                            case "UseCustomJavaPath":
-                                                useCustomJavaPath = bool.Parse(reader.Value.ToString());
-                                                break;
-                                            case "JavaPath":
-                                                if(reader.Value == null) CustomJavaPath = string.Empty;
-                                                else CustomJavaPath = reader.Value.ToString();
-                                                break;
-                                        }
-                                        reader.Read();
-                                    }
-                                    break;
-                                case "lastConnectedAccount":
-                                    if(reader.Value == null) LastConnectedAccount = string.Empty;
-                                    else LastConnectedAccount = reader.Value.ToString();
-                                    break;
-                                case "KonamiCodeEnabled":
-                                    MainWindow.KonamiCodeEnabled = true;
-                                    break;
+                                string sn = reader.Value.ToString();
+                                reader.Read();
+                                switch (sn)
+                                {
+                                    case "ScreenWidth": ScreenWidth = Convert.ToInt32(reader.Value); break;
+                                    case "ScreenHeight": ScreenHeight = Convert.ToInt32(reader.Value); break;
+                                    case "FullScreen": FullScreen = Convert.ToBoolean(reader.Value); break;
+                                }
+                                reader.Read();
                             }
-                        }
+                            break;
+                        case "CustomJavaPath":
+                            reader.Read();
+                            while (reader.TokenType != JsonToken.EndObject)
+                            {
+                                string jn = reader.Value.ToString();
+                                reader.Read();
+                                switch (jn)
+                                {
+                                    case "UseCustomJavaPath": useCustomJavaPath = Convert.ToBoolean(reader.Value); break;
+                                    case "JavaPath": CustomJavaPath = reader.Value?.ToString() ?? string.Empty; break;
+                                }
+                                reader.Read();
+                            }
+                            if (useCustomJavaPath) JavaPath = CustomJavaPath;
+                            break;
+                        case "lastConnectedAccount":
+                            LastConnectedAccount = reader.Value?.ToString() ?? string.Empty;
+                            break;
+                        case "KonamiCodeEnabled":
+                            MainWindow.KonamiCodeEnabled = true;
+                            break;
                     }
                 }
             }
+            RebuildJvmArgs();
         }
-        
-    }
-    
-    /*
-     * Java path
-     */
-    
-    public void EnableCustomJavaPath(bool enable)
-    {
-        useCustomJavaPath = enable;
-        if (enable)
+
+        public void EnableCustomJavaPath(bool enable)
         {
-            JavaPath = CustomJavaPath;
+            useCustomJavaPath = enable;
+            JavaPath = enable ? CustomJavaPath : null;
         }
-        else
+
+        public bool IsCustomJavaPathEnabled() => useCustomJavaPath;
+
+        public void UpdateSession()
         {
-            JavaPath = null;
+            Session = LauncherMain.GetInstance().GetSession();
         }
     }
-    
-    public bool IsCustomJavaPathEnabled()
-    {
-        return useCustomJavaPath;
-    }
-    
-    /*
-     * Session management
-     */
-    public void UpdateSession()
-    {
-        Session = LauncherMain.GetInstance().GetSession();
-    }
-    
 }
